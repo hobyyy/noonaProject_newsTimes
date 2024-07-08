0e0943b7a3b8e9705f00840a42f37a3052e3d9e5
EunseoKim <dmstj9408@gmail.com>
2024-07-08 22:08:24 +0900

    동작가능한 api로 수정

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-const api_Key = `170dea74e7a84e23bddd61f3f824eb1a`;
+// const api_Key = `170dea74e7a84e23bddd61f3f824eb1a`;
+const keyword = '데이식스';
+const PAGE_SIZE = 20;
 let news = [];
 const getLatestNew = async ()=> {
-  const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${api_Key}`);
+  // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${api_Key}`);
+  const url = new URL(`https://noonaproject-newstimes.netlify.app/top-headlines`);
+  // const url = new URL(`https://noona-times-v2.netlify.app/top-headlines`);
+  //?q=${keyword}&country=kr&pageSize=${PAGE_SIZE}
   const response = await fetch(url);
   const data = await response.json();
   news = data.articles;
